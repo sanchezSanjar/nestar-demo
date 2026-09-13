6cec5724723f6217ac47faf371dfe6010908c521
sanchezSanjar <sanjar@ajou.ac.kr>
2026-09-13 23:19:20 +0900

    feat: SASS integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,22 @@ import { NextPage } from "next";
 const Home:NextPage = () => {
   return (
     <>
-    <Container>
+    <Stack>
       <Stack flexDirection={"column"}>
-        <Box>Popular Properties</Box>
-        <Box>Top Agents</Box>
-        <Box>Top Properties</Box>
-        <Box>Events</Box>
+        <Stack>
+        <Stack className="container">Popular Properties</Stack>
+        </Stack>
+        <Stack>
+        <Stack className="container">Top Agents</Stack>
+        </Stack>
+        <Stack>
+        <Stack className="container">Top Properties</Stack>
+        </Stack>
+        <Stack>
+        <Stack className="container">Events</Stack>
+        </Stack>
       </Stack>
-    </Container>
+    </Stack>
     </>
   );
 } 

6dc74ba5e85fd29cb00e21ba8048ba5d32ae9e1b
sanchezSanjar <sanjar@ajou.ac.kr>
2026-09-14 01:58:26 +0900

    feat: publish main layout component

---
+++ libs/components/Footer.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "@mui/material"
+
+
+const Footer = () => {
+    return <Stack> Footer </Stack>;
+};
+
+export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import type { AppProps } from "next/app";
 import {ThemeProvider, createTheme} from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import {light} from "../scss/MaterialTheme"
-import { useState } from "react";
-import "../scss/app.scss";
+import { useState } from "react"; // @ts-ignore
+import "../scss/app.scss";    // @ts-ignore
+import "../scss/pc/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore

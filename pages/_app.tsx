23fdeb671789c0e78a1d044d8daff58cf546591b
sanchezSanjar <sanjar@ajou.ac.kr>
2026-09-14 18:55:33 +0900

    feat: develop apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,25 @@ import type { AppProps } from "next/app";
 import {ThemeProvider, createTheme} from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import {light} from "../scss/MaterialTheme"
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 import { useState } from "react"; // @ts-ignore
 import "../scss/app.scss";    // @ts-ignore
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss";
 
+
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
   const [theme, setTheme] = useState(createTheme(light));
 
-  // Socket.io Redux, Mui ...
+ // Socket.io, Redux, Mui, Apollo Client ...
   return (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <Component {...pageProps} />
-  </ThemeProvider>)
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ import withLayoutMain from "../libs/components/layout/LayoutHome";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import {Stack} from "@mui/material";
 import { NextPage } from "next";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/user/query";
 // import "swiper/css";
 // import "swiper/css/pagination";
 // import "swiper/css/navigation";
@@ -15,6 +17,26 @@ import { NextPage } from "next";
 
 const Home:NextPage = () => {
    const device = useDeviceDetect();
+
+   const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+  console.log("getPropertiesData =>", getPropertiesData);
+
 
   if (device === "mobile") {
     return <Stack>HOMEPAGE MOBILE</Stack>;
